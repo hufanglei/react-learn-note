0af0b041afe2e8ee5d935794c58c94c89ef3d97c
hfl <690328661@qq.com>
2022-09-08 06:34:40 +0800

    jsx语法糖

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,31 @@
-import React from "react";
+import React from "react"
+
+const msg = "你好世界111"
+
+const myStyle = {backgroundColor: 'skyblue'}
 
 //类组件
 class App extends React.Component {
     render() {
         return (
             <div>
-                <h2>你好世界</h2>
+                <h2>{msg}</h2>
                 <input type="text"/>
+                <hr/>
+                <div className="box">盒子</div>
+                <hr/>
+                <div style={myStyle}>内容内容</div>
             </div>
         )
     }
 }
 
 export default App
+
+/**
+ * 1.文件名可以是jsx或者js，不影响文件大小写
+ * 2.组件名称必须大写
+ * 3.js中出现（）代表其中想要写html
+ * 4.html中出现{}。代表其中要写js
+ * 5.其实export default也可以写到class前面
+ */
