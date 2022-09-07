86a0ef3c4f14d85f1ed652c7d7aac224ff065af6
hfl <690328661@qq.com>
2022-09-07 22:23:16 +0800

    hello-world

--- src/index.js
+++ src/index.js
@@ -1,17 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+// console.log('hello world')
+import ReactDOM from 'react-dom'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+//ReactDOM.render(组件名称，要注入的元素)
+ReactDOM.render(
+    <h2>你好</h2>,
+    document.getElementById('root')
+)
