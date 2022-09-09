a4e4a7c837ac458d6cf5955e9a9bf3871933d80a
hfl <690328661@qq.com>
2022-09-10 07:54:44 +0800

    setState的三种传参方式

--- src/App3.jsx
+++ src/App3.jsx
@@ -6,6 +6,7 @@ class App3 extends Component {
         this.state = {
             num: 1
         }
+        this.addNum = this.addNum.bind(this)
     }
     // state = {
     //     num: 1
@@ -15,9 +16,13 @@ class App3 extends Component {
             <div>
                 <h2>数字为: {this.state.num}</h2>
                 {/*<button onClick={ () => this.setState({num:2}) }>累加</button>*/}
-                <button onClick={ () => this.setState({num: this.state.num + 1}) }>累加</button>
-                //this传递方式1
-                <button onClick={this.addNum.bind(this) }>累加</button>
+                <button onClick={ () => this.setState({num: this.state.num + 1}) }>按钮 - 累加1</button>
+                {/*//this传递方式1*/}
+                <button onClick={this.addNum }>按钮 - 累加2</button>
+
+                {/*//this传递方式3*/}
+                <button onClick={() => this.addNum()}>按钮 - 累加3</button>
+
             </div>
         );
     }
