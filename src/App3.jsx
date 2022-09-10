15866b39c469f02faa30ad437ab533db6e28a9d3
hfl <690328661@qq.com>
2022-09-10 11:20:24 +0800

    子组件向父组件传值

--- src/App3.jsx
+++ src/App3.jsx
@@ -1,23 +1,41 @@
-import React from 'react';
+import React,{useState} from 'react';
 
 //子组件
 function Child(props){
-    return <h2>子组件 - {props.num}</h2>
+    return (
+        <>
+            <h2>子组件 - {props.num}</h2>
+            <button onClick={()=> props.setNum(789)}>修改num</button>
+        </>
+    )
 }
 
 //父组件
 function Father(props){
     console.log(props.num)
-    return <Child num={props.num} />
+    return <Child num={props.num} setNum={props.setNum} />
 }
 
 //顶级组件
 const App3 = () => {
+    const[num,setNum] = useState(123)
+    //提供给子组件用来修改num的函数
+    // const changeNumFn = () => setNum(456)
+    // const changeNumFn = (arg) => console.log(arg)
+    // const changeNumFn = (arg) => setNum(arg)
+
     return (
         <div>
-            <Father num={456}/>
+            {/*<Father num={num} changeNumFn={changeNumFn}></Father>*/}
+            <Father num={num} setNum={setNum}></Father>
         </div>
     );
 };
 
 export default App3;
+
+/**
+ * 无论是 vue 小程序 还是 react，所谓的子传父，
+ * 真实的在干活的是 父组件
+ *
+ */
