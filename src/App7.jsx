32bfaa5103aad49e48205493bffec8df1cd922bb
hfl <690328661@qq.com>
2022-09-10 14:22:44 +0800

    useCallBack 解决动态子组件的刷新问题

--- src/App7.jsx
+++ src/App7.jsx
@@ -1,23 +1,36 @@
-import React,{useState, memo} from 'react';
+import React,{useState, memo,useCallback} from 'react';
 
 // function Child(){
 //     console.log(123)
 //     return <div>子组件</div>
 // }
 
-const Child = memo(() => {
+const Child = memo((props) => {
     console.log(123)
-    return <div>子组件</div>
+    // return <button onClick={()=> props.setNum(props.num+1)}>累加</button>
+    return <button onClick={()=> props.doSth()}>累加</button>
 })
 
 const App7 = () => {
     const [num, setNum] = useState(1)
+
+    /**
+     * setNum(newValue) 使用新只强行覆盖初始值
+     * setNum((num)=>num+1)  不断使用新值覆盖旧值
+     * @type {function(): void}
+     */
+
+    //[] 代表不检测更新
+    const doSth = useCallback(() =>{
+            setNum(num => num+1)
+        },[])
     return (
         <div>
             <h3>数字为： {num}</h3>
-            <button onClick={()=>setNum(num+1)}>累加</button>
-            <hr/>
-            <Child />
+            {/*<button onClick={()=>setNum(num+1)}>累加</button>*/}
+            {/*<hr/>*/}
+            {/*<Child setNum={setNum} num={num}/>*/}
+            <Child doSth={doSth} />
         </div>
     );
 };
