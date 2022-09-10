96107b68a195d0dedf6ac5e76332dfc6f431da28
hfl <690328661@qq.com>
2022-09-10 08:53:58 +0800

    函数式组件的特点

--- src/App.jsx
+++ src/App.jsx
@@ -1,46 +1,37 @@
-import React from "react"
+// import React, {Component} from 'react';
+//
+// class App extends Component {
+//     render() {
+//         return (
+//             <div>
+//                 APP组件
+//             </div>
+//         );
+//     }
+// }
+//
+// export default App;
 
-const msg = "你好世界111"
+// export  default function App() {
+//     return <h2>App</h2>
+// }
 
-const myStyle = {backgroundColor: 'skyblue'}
-let flag = false;
-let arr = ['刘备', '关羽', '张飞']
-//类组件
-class App extends React.Component {
-    render() {
-        return (
-            <>
-                <h2>{msg}</h2>
-                {/* */}
-                <input type="text"/>
-                <hr/>
-                <div className="box">盒子</div>
-                <hr/>
-                <div style={myStyle}>1111</div>
-                <hr/>
-                <div style={{backgroundColor: flag ? 'skyblue':'pink'}}>内容内容</div>
-                <ul>
-                    {
-                        //React中的列表循环有且只有map可以使用
-                        //只有map才有返回值，forEach没有
-                        // arr.map((item,index)=>{
-                        //     return <li key={index}>{item}</li>
-                        // })
-                        arr.map((item,index) =>  <li key={index}>{item}</li>)
-                    }
-                </ul>
-            </>
-        )
-    }
+//es5写法
+//  function App() {
+//     return <h2>App</h2>
+// }
+
+//es6写法
+const App = () => {
+    console.log(this)
+    return <h2>App</h2>
 }
-
-export default App
+export default App;
 
 /**
- * 1.文件名可以是jsx或者js，不影响文件大小写
- * 2.组件名称必须大写
- * 3.js中出现（）代表其中想要写html
- * 4.html中出现{}。代表其中要写js
- * 5.其实export default也可以写到class前面
+ * 1. 函数式组件没有生命周期
+ * 2. 函数式组件没有this
+ * 3. 函数式组件没有state状态
  *
- **/
+ * Hooks
+ */
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 // console.log('hello world')
 import ReactDOM from 'react-dom'
-import App from "./App4";
+import App from "./App";
 
 //ReactDOM.render(组件名称，要注入的元素)
 ReactDOM.render(
