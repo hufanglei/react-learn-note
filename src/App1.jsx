34217b1f867427ccf8975907782d7c788a339e3d
hfl <690328661@qq.com>
2022-09-10 09:32:45 +0800

    函数累加

--- src/App1.jsx
+++ src/App1.jsx
@@ -5,20 +5,13 @@ let msg = "你好世界"
 //触发视图更新的东西 useState
 function App1(){
     //Hook只能用在组件函数中的最顶层
-    const [msg,setMsg] = useState('你好世界')
-    console.log(msg)
-    const fn = () => {
-        // msg = 'hello world'
-        // console.log(msg)
-        setMsg('hello world')
-    }
+    const [num,setNum] = useState(1)
+
 
     return (
         <>
-        <h2>{msg}</h2>
-        {/*<button onClick={()=>msg="hello world"}>修改</button>*/}
-        <button onClick={fn}>修改</button>
-        <button onClick={() => setMsg('hello world')}>修改msg2</button>
+          <h2>{num}</h2>
+           <button onClick={() => setNum(num+1)}>累加</button>
         </>
     )
 }
